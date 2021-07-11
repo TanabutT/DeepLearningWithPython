40e8322fd96e797c32e1bb1fa4f4d65503ef0a1f
TanabutT <t_tanabut@hotmail.com>
2021-07-11 21:11:14 +0700

    Create Batch_size_and_BatchNormalization_is_help.py

---
+++ Batch_size_and_BatchNormalization_is_help.py
@@ -0,0 +1,36 @@
+model = get_model()
+
+# Fit your model for 5 epochs with a batch of size the training set
+model.fit(X_train, y_train, epochs=5, batch_size=X_train.shape[0])
+print("\n The accuracy when using the whole training set as batch-size was: ",
+      model.evaluate(X_test, y_test)[1])
+
+
+
+
+
+# Import batch normalization from keras layers
+from keras.layers import BatchNormalization
+
+# Build your deep network
+batchnorm_model = Sequential()
+batchnorm_model.add(Dense(50, input_shape=(64,), activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(50, activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(50, activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(10, activation='softmax', kernel_initializer='normal'))
+
+# Compile your model with sgd
+batchnorm_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train your standard model, storing its history callback
+h1_callback = standard_model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs=10, verbose=0)
+
+# Train the batch normalized model you recently built, store its history callback
+h2_callback = batchnorm_model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs=10, verbose=0)
+
+# Call compare_histories_acc passing in both model histories
+compare_histories_acc(h1_callback, h2_callback)
+
